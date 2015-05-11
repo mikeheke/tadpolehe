14bed9707926311c6aa93b2c1e025cef58b5c291
Mike He <ke_he@163.com>
2015-05-11 18:40:56 +0800

    tadpolehe fwk refactor.  modify by Mike

--- tadpolehe-core/src/main/resources/job/job.bat
+++
@@ -1,3 +0,0 @@
-start iexplore.exe http://afw-j-uat.intranet.local:8080/sysManage/jobTriggerAction.action?jobCode=testJob
-#ping 127.1 -n 60 >nul
-#if exist iexplore.exe taskkill /f /im iexplore.exe
